b11d1d35da38084f6ad8964703354274b34475db
Дмитрий Цебинога <tsebik@ukr.net>
2022-07-21 18:00:48 +0300

    practical

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,37 @@
 import Vimeo from '@vimeo/player';
 
-const LOCAL_KEY = 'videoplayer-current-time';
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
+getLocalStorage();
+
 player.on('timeupdate', function (data) {
   const timePlayer = data.seconds;
 
-  localStorage.setItem(LOCAL_KEY, timePlayer);
+  localStorage.setItem(STORAGE_KEY, timePlayer);
 });
+
+function getLocalStorage() {
+  const valueLocalStorage = localStorage.getItem(STORAGE_KEY);
+
+  if (valueLocalStorage) {
+    player
+      .setCurrentTime(valueLocalStorage)
+      .then(function (seconds) {
+        seconds = valueLocalStorage;
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
+}
